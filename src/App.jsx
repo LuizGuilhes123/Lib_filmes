886e732c07e15a56014fe3843294731d6073b574
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-01-10 09:13:36 -0300

    feat: novas paginas adicionadas #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <nav id="navbar">
+        <h2>
+          <Link to="/">Movies Lib</Link>
+        </h2>
+        <Link to="/Mmovie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
       <h2>Lib Movies</h2>
     </div>
   )
