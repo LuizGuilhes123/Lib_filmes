55bc6838fccc5e4f93d9d00998010b58defaf961
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-01-10 09:45:01 -0300

    feat: novos componentes foram adicionados #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className='App'>
-      <nav id="navbar">
-        <h2>
-          <Link to="/">Movies Lib</Link>
-        </h2>
-        <Link to="/Mmovie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <Navbar/>
       <h2>Lib Movies</h2>
+      <Outlet/>
     </div>
   )
 }
